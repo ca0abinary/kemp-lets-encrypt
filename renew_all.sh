21bef05b461b636c902a9ab110493c38d5ba6bc2
Jonathan DeMarks <ca0abinary@users.noreply.github.com>
2017-03-21 08:01:27 -0400

    Update renew_all.sh

--- renew_all.sh
+++ renew_all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 find /etc/letsencrypt/configs/ -iname "*.pre.sh" -exec {} \;
-find /etc/letsencrypt/configs/ -iname "*.conf" -exec certbot --config {} --non-interactive certonly \;
+find /etc/letsencrypt/configs/ -iname "*.conf" -exec certbot --config {} --expand --non-interactive certonly \;
 find /etc/letsencrypt/configs/ -iname "*.post.sh" -exec {} \;
